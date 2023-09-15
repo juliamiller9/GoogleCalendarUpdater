861964738e9fd34d94a569df023b4f207e9a1195
Julia Miller <juliam00@mail.uwmcf.uw.edu>
2023-09-15 13:12:25 -0700

    renamed eventids and added import time

--- update.py
+++ update.py
@@ -6,6 +6,7 @@ from __future__ import print_function
 
 import datetime
 import os.path
+import time
 
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
